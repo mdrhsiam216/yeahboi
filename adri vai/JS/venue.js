cad8b707b4a963d6573463aa74ea456e3169bd0a
mdrhsiam216 <58131237+mdrhsiam216@users.noreply.github.com>
2023-01-26 10:39:31 -0800

    Add files via upload

---
+++ adri vai/JS/venue.js
@@ -0,0 +1,18 @@
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5500,
+      disableOnInteraction: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
